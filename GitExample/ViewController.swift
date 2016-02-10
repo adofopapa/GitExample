ef5885f732217ff0edf41bae77bb474e347b25f1
Papa Boateng <Papaadofo@node-1w7jr9y5luk5z7hzy6oa2sfce.ipv6.telus.net>
2016-02-10 00:33:18 -0700

    Extra comment

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -18,6 +18,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //change here okay
+        //one time GIT
+        //time
+        
     }
 
 
